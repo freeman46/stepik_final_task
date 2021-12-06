2c3661ed86498a26deef97b566a2cd5747897d82
VictorAlexandrov <antifreeman@gmail.com>
2021-12-06 15:58:09 +0300

    Добавление параметризации в тест для проверки всех вариантов '?promo=offerN' в ссылке

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,26 @@
+import pytest
+
 from .pages.product_page import ProductPage
 
+promo_list = [
+    '?promo=newYear',
+    '?promo=newYear2019',
+    '?promo=offer0',
+    '?promo=offer1',
+    '?promo=offer2',
+    '?promo=offer3',
+    '?promo=offer4',
+    '?promo=offer5',
+    '?promo=offer6',
+    '?promo=offer7',
+    '?promo=offer8',
+    '?promo=offer9'
+]
 
-def test_guest_can_add_product(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+
+@pytest.mark.parametrize('promo', promo_list)
+def test_guest_can_add_product(browser, promo):
+    link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/{promo}'
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.should_be_product_page()
-
